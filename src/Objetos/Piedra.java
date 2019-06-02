0f8630c359db21e4a2876605ac23c9e352559c33
Hugo Larrea <hugo.larrea@hotmail.com>
2019-06-02 15:35:12 -0300

    Arreglo de error en Piedra

--- src/Objetos/Piedra.java
+++ src/Objetos/Piedra.java
@@ -3,7 +3,6 @@ package Objetos;
 public class Piedra extends Material{
     public Piedra (){
         this.durabilidad =200;
-        this.fureza = 5;
     }
 
 }
--- test/AlgoCraftTests.java
+++ test/AlgoCraftTests.java
@@ -2,9 +2,9 @@ import Objetos.Hacha;
 import Objetos.Madera;
 import Objetos.Metal;
 import Objetos.Piedra;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class AlgoCraftTests {
 
