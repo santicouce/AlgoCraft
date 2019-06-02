17ebfe0967c2d8e159bb6a45ba3eb05736ba0c32
Hugo Larrea <hugo.larrea@hotmail.com>
2019-06-02 16:23:12 -0300

    Primera iteracion TDD de HachaTest

--- src/Herramienta.java
+++
@@ -1,3 +0,0 @@
-public class Herramienta {
-    private int Duracion = 0;
-}
---
+++ src/Objetos/Hacha.java
@@ -0,0 +1,16 @@
+package Objetos;
+
+public class Hacha {
+
+    public Hacha(Madera madera){
+
+    }
+
+    public int durabilidad() {
+        return 100;
+    }
+
+    public int fuerza(){
+        return 2;
+    }
+}
