1a867fc705d92867b35ec4731bd16b1fd5f6d285
Hugo Larrea <hugo.larrea@hotmail.com>
2019-06-08 14:52:58 -0300

    Creacion de Duracion

--- src/Objetos/Herramienta.java
+++ src/Objetos/Herramienta.java
@@ -6,15 +6,14 @@ public abstract class Herramienta {
     protected int durabilidad;
     protected int fuerza;
     protected Desgaste desgaste;
+    protected Duracion duracion;
 
     public int durabilidad() {
         return this.durabilidad;
     }
-
     public int fuerza(){
         return this.fuerza;
     }
-
     public abstract void golpear(Material material);
-
+    public void rompete(){durabilidad = 0;}
 }
