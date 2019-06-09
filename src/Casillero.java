69f7130fa9d20a6282da57ddd7ad4e491ac8bd66
Altamirano Giuliana <giuliana.e.altamirano@gmail.com>
2019-06-09 04:05:52 -0300

    Hice cambios a Casillero

--- src/Casillero.java
+++ src/Casillero.java
@@ -1,10 +1,17 @@
 public class Casillero {
 
     private boolean estado;
+    private Object contenedor;
 
     public  Casillero(){ }
 
     public boolean esUnCasillero() {
         return true;
     }
+
+/*    public void setContenedor(Object objeto){
+
+    }
+*/
 }
+ 
