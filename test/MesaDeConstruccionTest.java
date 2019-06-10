d880d1d95fde1ad2030df4ae52e804c837c2d5f9
santicouce <santicouce@outlook.com>
2019-06-10 19:43:08 -0300

    agrego pruebas de MesaDeConstruccion(aun no lo termine)

--- test/MesaDeConstruccionTest.java
+++ test/MesaDeConstruccionTest.java
@@ -1,4 +1,32 @@
 package PACKAGE_NAME;
 
+import AlgoCraft.MesaDeConstruccion;
+import AlgoCraft.Piedra;
+import org.junit.Test;
+
 public class MesaDeConstruccionTest {
+    @Test
+    public void test01MesaDeConstruccionTieneTamanio3x3(){
+        MesaDeConstruccion mesaDeConstruccion = new MesaDeConstruccion();
+        mesaDeConstruccion.validarPosicion(0,0);
+        mesaDeConstruccion.validarPosicion(0,1);
+        mesaDeConstruccion.validarPosicion(0,2);
+        mesaDeConstruccion.validarPosicion(1,0);
+        mesaDeConstruccion.validarPosicion(1,1);
+        mesaDeConstruccion.validarPosicion(1,2);
+        mesaDeConstruccion.validarPosicion(2,0);
+        mesaDeConstruccion.validarPosicion(2,1);
+        mesaDeConstruccion.validarPosicion(2,2);
+    }
+
+    @Test
+    public void test02PuedoAniadirUnMaterialEnUnaPosicionValida(){
+        MesaDeConstruccion mesaDeConstruccion = new MesaDeConstruccion();
+        Piedra piedra1 = new Piedra();
+        Piedra piedra2 = new Piedra();
+        mesaDeConstruccion.aniadirElementoEnPosicion(piedra1,1,2);
+        mesaDeConstruccion.aniadirElementoEnPosicion(piedra2,1,1);
+        //para chequear que realmente se agrego podria ver que el inventario tengo un material menos
+        }
+
 }
