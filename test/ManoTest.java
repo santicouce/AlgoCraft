70851c615bfd60157b8c7691d93966bc9e46643c
Hugo Larrea <hugo.larrea@hotmail.com>
2019-06-10 17:35:57 -0300

    Travis Dame El Check

--- test/ManoTest.java
+++ test/ManoTest.java
@@ -1,11 +1,12 @@
-import AlgoCraft.Hacha;
 import AlgoCraft.Mano;
-import AlgoCraft.Madera;
 import org.junit.Test;
 
 public class ManoTest {
 
     private Mano mano = new Mano();
-
+    @Test
+    public void test01UnaManoDeberiaComenzarVacia(){
+        Mano mano = new Mano();
+    }
 
 }
