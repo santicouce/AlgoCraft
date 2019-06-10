0079c0f59c73b094fc1d4f15244293599bb84bb3
Altamirano Giuliana <giuliana.e.altamirano@gmail.com>
2019-06-10 00:22:49 -0300

    Agregué clases Direccion, ubicacion y MesaDeConstruccion

---
+++ src/AlgoCraft/MesaDeConstruccion.java
@@ -0,0 +1,5 @@
+package AlgoCraft;
+
+public class MesaDeConstruccion {
+    
+}
---
+++ src/AlgoCraft/Ubicacion.java
@@ -0,0 +1,22 @@
+package AlgoCraft;
+
+public class Ubicacion {
+    private int columna;
+    private int fila;
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+}
