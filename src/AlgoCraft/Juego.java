882b843410bfad0b201daffb09db9f760fb55685
giualtamirano <giuliana.e.altamirano@gmail.com>
2019-06-11 18:10:04 -0300

    agregué Juego y JuegoTest

---
+++ src/AlgoCraft/Juego.java
@@ -0,0 +1,23 @@
+package AlgoCraft;
+
+public class Juego {
+    private Mapa mapa = new Mapa();
+    private Jugador jugador = new Jugador();
+
+    public Juego(){
+        Madera madera = new Madera();
+        Piedra piedra = new Piedra();
+        Metal metal = new Metal();
+        Diamante diamante = new Diamante();
+
+        mapa.aniadirElementoEnPosicion(madera , 2,2);
+        mapa.aniadirElementoEnPosicion(madera , 2,3);
+        mapa.aniadirElementoEnPosicion(diamante , 3,2);
+        mapa.aniadirElementoEnPosicion(metal , 3,3);
+        mapa.aniadirElementoEnPosicion(piedra , 5,2);
+        mapa.aniadirElementoEnPosicion(madera , 7,3);
+        mapa.aniadirElementoEnPosicion(metal , 4,1);
+
+        jugador.aniadirJugadorAlMapa(5,5);
+    }
+}
---
+++ test/JuegoTest.java
@@ -0,0 +1,10 @@
+import AlgoCraft.Juego;
+import org.junit.Test;
+
+public class JuegoTest {
+    private Juego juego = new Juego();
+    @Test
+    public void test01UnaManoDeberiaComenzarVacia(){
+        Juego juego = new Juego();
+    }
+}
