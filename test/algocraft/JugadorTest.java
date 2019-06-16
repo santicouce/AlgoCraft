ed946c685c1b5a330791bd8046dfd9e6370f8c84
Hugo Larrea <hugo.larrea@hotmail.com>
2019-06-16 00:37:19 -0300

    Jugador lanza MovimientoInvalidoError

--- test/algocraft/JugadorTest.java
+++ test/algocraft/JugadorTest.java
@@ -1,5 +1,6 @@
 package algocraft;
 
+import algocraft.errores.MovimientoInvalidoError;
 import algocraft.jugador.Jugador;
 import algocraft.movimientodeljugador.*;
 import algocraft.materiales.Madera;
@@ -12,15 +13,15 @@ public class JugadorTest {
     private Jugador jugador = new Jugador();
     Mapa mapa = new Mapa();
 
-    //@Test
-    //public void test01JugadorRecienCreadoCuentaSoloConUnHachaDeMadera(){
-      //  Madera madera = new Madera();
-       // int durabilidadInicialMadera = madera.durabilidad();
-        //jugador.golpear(madera);
-        //assertEquals(durabilidadInicialMadera - 2, madera.durabilidad());
-    //}
     @Test
-    public void test02UnJugadorSeIntentaMoverALaIzquieraAUnCasilleroOcupadoPorUnMaterialYNoDeberiaCambiarSuPosicion(){
+    public void test01JugadorRecienCreadoDeberiaCuentaSoloConUnHachaDeMadera(){
+        Madera madera = new Madera();
+        int durabilidadInicialMadera = madera.durabilidad();
+        jugador.golpear(madera);
+        assertEquals(durabilidadInicialMadera - 2, madera.durabilidad());
+    }
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test02UnJugadorSeIntentaMoverALaIzquieraAUnCasilleroOcupadoPorUnMaterialYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirElementoEnPosicion(new Madera(),0,0);
         mapa.aniadirJugadorAlMapa(jugador,1,0);
         jugador.moverA(new DireccionIzquierda(mapa));
@@ -31,20 +32,20 @@ public class JugadorTest {
         jugador.aniadirJugadorAlMapa(1,0);
         jugador.moverA(new DireccionIzquierda(mapa));
     }
-    @Test
-    public void test04UnJugadorIntentaMoverseALaIzquierdaEstandoEnElBordeIzquierdoDelMapaYSuPosicionNoDeberiaCambiar(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test04UnJugadorIntentaMoverseALaIzquierdaEstandoEnElBordeIzquierdoDelMapaYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirElementoEnPosicion(jugador,0,0);
         jugador.aniadirJugadorAlMapa(0,0);
         jugador.moverA(new DireccionIzquierda(mapa));
     }
-    @Test
-    public void test05UnJugadorIntentaMoverseALaDerechaAUnCasilleroOcupadoPorUnMaterialYNoDeberiaCambiarSuPosicion(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test05UnJugadorIntentaMoverseALaDerechaAUnCasilleroOcupadoPorUnMaterialYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirElementoEnPosicion(new Madera(),1,0);
         mapa.aniadirJugadorAlMapa(jugador,0,0);
         jugador.moverA(new DireccionDerecha(mapa));
     }
-    @Test
-    public void test06UnJugadorIntentaMoverseALaDerechaEstandoEnElBordeDerechoDelMapaYSuPosicionNoDeberiaCabiar(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test06UnJugadorIntentaMoverseALaDerechaEstandoEnElBordeDerechoDelMapaYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirJugadorAlMapa(jugador,9,0);
         jugador.moverA(new DireccionDerecha(mapa));
     }
@@ -53,14 +54,14 @@ public class JugadorTest {
         mapa.aniadirJugadorAlMapa(jugador,0,0);
         jugador.moverA(new DireccionDerecha(mapa));
     }
-    @Test
-    public void test08UnJugadorIntentaMoverseArribaAUnCasillerOcupadoYSuPosicionNoDeberiaCambiar(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test08UnJugadorIntentaMoverseArribaAUnCasillerOcupadoYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirElementoEnPosicion(new Madera(),0 ,0);
         mapa.aniadirJugadorAlMapa(jugador,0,1);
         jugador.moverA(new DireccionArriba(mapa));
     }
-    @Test
-    public void test09UnJugadorIntentaMoverseArribaEstandoEnElBordeSuperiorDelMapaYSuPosicionNoDeberiaCambiar(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test09UnJugadorIntentaMoverseArribaEstandoEnElBordeSuperiorDelMapaYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirJugadorAlMapa(jugador,0,0);
         jugador.moverA(new DireccionArriba(mapa));
     }
@@ -69,14 +70,14 @@ public class JugadorTest {
         mapa.aniadirJugadorAlMapa(jugador,0,2);
         jugador.moverA(new DireccionArriba(mapa));
     }
-    @Test
-    public void test11UnJugadorIntentaMoverseAbajoAUnCasilleroOcupadoYSuPosicionNoDeberiaCambiar(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test11UnJugadorIntentaMoverseAbajoAUnCasilleroOcupadoYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirElementoEnPosicion(new Madera(),0 ,1);
         mapa.aniadirJugadorAlMapa(jugador,0,0);
         jugador.moverA(new DireccionAbajo(mapa));
     }
-    @Test
-    public void test12UnJugadorIntentaMoverseAbajoEnElBordeInferiorDelMapaYSuPosicionNoDeberiaCambiar(){
+    @Test(expected = MovimientoInvalidoError.class)
+    public void test12UnJugadorIntentaMoverseAbajoEnElBordeInferiorDelMapaYSeDeberiaLanzarMovimientoInvalidoError(){
         mapa.aniadirJugadorAlMapa(jugador, 0,9);
         jugador.moverA(new DireccionAbajo(mapa));
     }
