c81e23d22c8139acf62c3e5b16f09d68481001bb
Hugo Larrea <hugo.larrea@hotmail.com>
2019-06-18 02:58:35 -0300

    Creacion de mapa. Inicializa lleno de madera

--- src/algocraft/interfazgrafica/vista/ContenedorJuego.java
+++ src/algocraft/interfazgrafica/vista/ContenedorJuego.java
@@ -2,9 +2,11 @@ package algocraft.interfazgrafica.vista;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -12,7 +14,7 @@ public class ContenedorJuego extends BorderPane{
 
     BarraDeMenu menuBar;
     Canvas canvasCentral;
-    VBox contenedorCentral;
+    GridPane contenedorCentral;
 
     public ContenedorJuego(Stage stage) {
         this.inicializarMenu(stage);
@@ -42,15 +44,28 @@ public class ContenedorJuego extends BorderPane{
 
     private void inicializarCentro() {
 
-        canvasCentral = new Canvas(460, 220);
+        int length = 16;
+        int width = 27;
 
-        contenedorCentral = new VBox(canvasCentral);
-        contenedorCentral.setAlignment(Pos.CENTER);
-        contenedorCentral.setSpacing(20);
-        contenedorCentral.setPadding(new Insets(25));
+        contenedorCentral = new GridPane();
+
+        for(int y = 0; y < length; y++){
+            for(int x = 0; x < width; x++){
+
+                Image imagen = new Image("file:src/algocraft/interfazgrafica/vista/imagenes/madera1.png");
+                ImageView tf = new ImageView();
+                tf.setFitWidth(50);
+                tf.setFitHeight(50);
+                tf.setImage(imagen);
+
+                contenedorCentral.setRowIndex(tf,y);
+                contenedorCentral.setColumnIndex(tf,x);
+                contenedorCentral.getChildren().add(tf);
+            }
+        }
         Image imagen = new Image("file:src/algocraft/interfazgrafica/vista/imagenes/pasto.jpg");
         BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        contenedorCentral.setBackground(new Background(imagenDeFondo));
+        this.setBackground(new Background(imagenDeFondo));
         this.setCenter(contenedorCentral);
     }
 }
